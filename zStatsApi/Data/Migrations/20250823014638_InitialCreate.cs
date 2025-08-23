6bc6d638801c91d149d64c2e81405b5113fbdc58
clee24 <clee24@wooster.edu>
2025-08-22 22:15:27 -0400

    Db Migration Files

---
+++ zStatsApi/Data/Migrations/20250823014638_InitialCreate.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace zStatsApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Nickname = table.Column<string>(type: "text", nullable: true),
+                    Gender = table.Column<string>(type: "text", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    TeamAId = table.Column<int>(type: "integer", nullable: false),
+                    TeamBId = table.Column<int>(type: "integer", nullable: false),
+                    WinnerTeamId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matches_Teams_TeamAId",
+                        column: x => x.TeamAId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Matches_Teams_TeamBId",
+                        column: x => x.TeamBId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Matches_Teams_WinnerTeamId",
+                        column: x => x.WinnerTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamPlayers",
+                columns: table => new
+                {
+                    TeamId = table.Column<int>(type: "integer", nullable: false),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamPlayers", x => new { x.TeamId, x.PlayerId });
+                    table.ForeignKey(
+                        name: "FK_TeamPlayers_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeamPlayers_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MatchId = table.Column<int>(type: "integer", nullable: false),
+                    SetNumber = table.Column<int>(type: "integer", nullable: false),
+                    TeamAScore = table.Column<int>(type: "integer", nullable: false),
+                    TeamBScore = table.Column<int>(type: "integer", nullable: false),
+                    WinnerTeamId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sets_Matches_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "Matches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sets_Teams_WinnerTeamId",
+                        column: x => x.WinnerTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PlayerStats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false),
+                    SetId = table.Column<int>(type: "integer", nullable: false),
+                    HittingKills = table.Column<int>(type: "integer", nullable: false),
+                    HittingErrors = table.Column<int>(type: "integer", nullable: false),
+                    HittingAttempts = table.Column<int>(type: "integer", nullable: false),
+                    ServiceAces = table.Column<int>(type: "integer", nullable: false),
+                    ServiceErrors = table.Column<int>(type: "integer", nullable: false),
+                    ServiceAttempts = table.Column<int>(type: "integer", nullable: false),
+                    SettingDimes = table.Column<int>(type: "integer", nullable: false),
+                    SettingErrors = table.Column<int>(type: "integer", nullable: false),
+                    SettingAttempts = table.Column<int>(type: "integer", nullable: false),
+                    Blocks = table.Column<int>(type: "integer", nullable: false),
+                    Digs = table.Column<int>(type: "integer", nullable: false),
+                    Shanks = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerStats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PlayerStats_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayerStats_Sets_SetId",
+                        column: x => x.SetId,
+                        principalTable: "Sets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_TeamAId",
+                table: "Matches",
+                column: "TeamAId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_TeamBId",
+                table: "Matches",
+                column: "TeamBId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matches_WinnerTeamId",
+                table: "Matches",
+                column: "WinnerTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerStats_PlayerId",
+                table: "PlayerStats",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerStats_SetId",
+                table: "PlayerStats",
+                column: "SetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_MatchId",
+                table: "Sets",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sets_WinnerTeamId",
+                table: "Sets",
+                column: "WinnerTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamPlayers_PlayerId",
+                table: "TeamPlayers",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerStats");
+
+            migrationBuilder.DropTable(
+                name: "TeamPlayers");
+
+            migrationBuilder.DropTable(
+                name: "Sets");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Matches");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
